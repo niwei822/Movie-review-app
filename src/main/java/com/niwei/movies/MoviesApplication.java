5dad6e5bedd72c2626b21b183aed0cb6174cd251
niwei822 <cecily.li.822@gmail.com>
2023-10-03 22:26:56 -0700

    set up mongodb database

--- src/main/java/com/niwei/movies/MoviesApplication.java
+++ src/main/java/com/niwei/movies/MoviesApplication.java
@@ -2,12 +2,24 @@ package com.niwei.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@RestController
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
+	
+	/*@GetMapping("/root")
+	public String apiRoot() {
+		return "Hello world";
+	}*/
+
+
+
+
 
 }
